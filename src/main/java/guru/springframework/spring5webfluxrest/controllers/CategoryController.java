689238c8d0ec3bda88777947bd99ef714493d5a7
Connor Wheatley <cwheatley@tier2consulting.com>
2022-01-31 09:59:04 +0000

    Added put operation for category

--- src/main/java/guru/springframework/spring5webfluxrest/controllers/CategoryController.java
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/CategoryController.java
@@ -36,5 +36,11 @@ public class CategoryController {
     public Mono<Void> createCategory(@RequestBody Publisher<Category> categoryStream) {
         return categoryRepository.saveAll(categoryStream).then();
     }
+
+    @PutMapping("/api/v1/categories/{id}")
+    public Mono<Category> updateCategory(@PathVariable String id, @RequestBody Category category) {
+        category.setId(id);
+        return categoryRepository.save(category);
+    }
 }
 
